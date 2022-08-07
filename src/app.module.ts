f74b4f4a8cb4a72df1d48659a009551c63718c6d
artur.tsoklan <a.tsoklan@gmail.com>
2022-08-07 02:51:25 +0200

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
+import { User } from './entity/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'Organization_app',
+      port: 5432 ?? process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
       synchronize: true,
+      migrations: ['./migrations/*.ts'],
     }),
     UsersModule,
     AuthModule,
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
+import { User } from '../entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
